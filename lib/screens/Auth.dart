97975cc394b5b13ad814704284f8b4f3e16f2325
onatcakr <onatcakr@gmail.com>
2020-05-26 16:12:07 +0300

    Splash screen and sign in with google function added

---
+++ lib/screens/Auth.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:group_expense/screens/CreateGroup.dart';
+import 'package:group_expense/screens/GroupPage.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            gradient: SweepGradient(
+              colors: [
+                Colors.white,
+                Colors.blueAccent,
+                Colors.white,
+                Colors.blueAccent,
+                Colors.white
+              ],
+              stops: [0.0, 0.25, 0.5, 0.75, 1],
+            ),
+          ),
+        ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              GoogleSignInButton(
+                onPressed: () async {
+                  try {
+                    setState(() {
+                      loading = true;
+                    });
+                    var user = await signIn();
+                    var result = await Firestore.instance
+                        .collection("groups")
+                        .where("userIds", arrayContains: user.uid)
+                        .getDocuments();
+                    if (result.documents.isEmpty) {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (BuildContext context) {
+                          return CreateGroup();
+                        }),
+                      );
+                    } else {
+                      var group = result.documents[0];
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (BuildContext context) {
+                          return GroupPage(
+                            group: group,
+                          );
+                        }),
+                      );
+                    }
+                  } catch (e) {
+                    setState(() {
+                      loading = false;
+                    });
+                    print(e);
+                  }
+                },
+                darkMode: true,
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Future<FirebaseUser> signIn() async {
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    print("signed in " + user.displayName);
+    return user;
+  }
+}
---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:group_expense/screens/Auth.dart';
+import 'package:group_expense/screens/CreateGroup.dart';
+import 'package:group_expense/screens/GroupPage.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  checkCurrentUser() async {
+    var user = await FirebaseAuth.instance.currentUser();
+    if (user != null) {
+      var result = await Firestore.instance
+          .collection("groups")
+          .where("userIds", arrayContains: user.uid)
+          .getDocuments();
+
+      if (result.documents.isEmpty) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (BuildContext context) {
+            return CreateGroup();
+          }),
+        );
+      } else {
+        var group = result.documents[0];
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (BuildContext context) {
+            return GroupPage(
+              group: group,
+            );
+          }),
+        );
+      }
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (BuildContext context) {
+          return Login();
+        }),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkCurrentUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
